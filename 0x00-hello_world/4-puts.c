c0c4ffd17496d00d3ece00ef1b9c5d6d12b3ca74
Bernard Kortor <benardkortor@gmail.com>
2022-12-09 12:35:26 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main-entry point
  * Return: always 0 success
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
