afe47c95c0484ea24866fb3c406d90c6d5052e04
Bernard Kortor <benardkortor@gmail.com>
2022-12-13 01:05:37 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program that print all numbers of base 16
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 48; /* 48. decimal rep of 0*/
+
+	while (digit <= 102) /*102, decimal rep of f */
+	{
+		putchar(digit);
+		/*after 9, will jump till 96 */
+		if (digit ==57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
