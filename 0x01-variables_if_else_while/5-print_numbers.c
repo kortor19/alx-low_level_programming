f872ccc419cf5484b915253d2e5d95af444a31e4
Bernard Kortor <benardkortor@gmail.com>
2022-12-12 23:22:21 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - 
+ * main - program that prints all single digit numbers
+ *
+ * Return: Always 0 (SUccess)
+ */
+
+int main(void)
+{
+	int firstDigit = 0, secondDigit;
+
+	while (firstDigit <= 99)
+	{
+		secondDigit = firstDigit;
+		while (secondDigit <= 99)
+		{
+			if (secondDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((secondDigit / 10) + 48);
+				putchar((secondDigit % 10) + 48);
+
+				if (firstDigit != 98 || secondDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			+secondDigit;
+		}
+		++firstDigit;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
