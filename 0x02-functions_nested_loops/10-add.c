1971faa3ec02e9a4686af5d40a9aa5d8fbd59e1c
Bernard Kortor <benardkortor@gmail.com>
2022-12-15 20:01:18 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - program that add two integers and returns result
+ * @num1: first number to be added
+ * @num2: second number to be added
+ *
+ * Return: result of the addition
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
