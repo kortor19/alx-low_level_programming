c790b4fe4ed364d0347c2abbf4e77a83b351e85a
Bernard Kortor <benardkortor@gmail.com>
2023-01-03 00:18:05 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, src_len = 0;
 
-	for (i = 0; i < n && *(src + i); i++)
-	{
-		*(dest + 1) = *(src + 1);
-	}
-	for (; i < n; i++)
-	{
-		*(dest + i) = '\0';
-	}
+	while (src[index++])
+		src_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 	return (dest);
 }
